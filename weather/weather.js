6f18503a9f5be5d54fcc8a19a1c56e4bdb8be13f
landry98 <78122910+landry98@users.noreply.github.com>
2021-04-07 22:32:13 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -16,7 +16,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
   if (feature.properties.severity === 'Severe') alertColor = 'red';
   return { color: alertColor };
 },
-   
+
 onEachFeature: function(feature, layer) {
   layer.bindPopup(feature.properties.headline);
     }
