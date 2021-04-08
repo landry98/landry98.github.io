04306f287a2da2fbf28ddca5a2108aea9d85feba
landry98 <78122910+landry98@users.noreply.github.com>
2021-04-07 22:31:25 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -16,13 +16,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
   if (feature.properties.severity === 'Severe') alertColor = 'red';
   return { color: alertColor };
 },
-    L.geoJSON(data, {
-  style: function(feature){
-  var alertColor = 'orange';
-  if (feature.properties.severity === 'Minor') alertColor = 'purple';
-  return { color: alertColor };
-},           
-      
+   
 onEachFeature: function(feature, layer) {
   layer.bindPopup(feature.properties.headline);
     }
